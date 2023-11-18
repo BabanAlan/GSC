9b5529370a150bf01b7dfe2019c09dc43e65c7a1
BabanAlan <99186635+BabanAlan@users.noreply.github.com>
2023-11-19 00:35:36 +0300

    Add files via upload

---
+++ categories.py
@@ -0,0 +1,68 @@
+fcsgo_category = [
+    "ELO",
+    "Matches / Won / Lost",
+    "Win rate",
+    "K / D",
+    "HS rate",
+    "Longest win streak",
+    "K - A - D",
+    "K / R",
+    "Win rate",
+    "K / D",
+    "HS rate",
+    "Current win streak",
+]
+
+osu_category = [
+    "pp",
+    "Hit Accuracy ",
+    "Maximum Combo",
+    "Global Rank",
+    "Country Rank",
+    "SS",
+    "SSH",
+    "S",
+    "SH",
+    "A",
+    "Ranked Score",
+    "Play Count",
+    "Total Score",
+    "Total Hits",
+    "Level",
+]
+
+osu_stats_category = [
+    "pp&quot",
+    "hit_accuracy&quot",
+    "maximum_combo&quot",
+    "global_rank&quot",
+    "country_rank&quot",
+    "ss&quot",
+    "ssh&quot",
+    "s&quot",
+    "sh&quot",
+    "a&quot",
+    "ranked_score&quot",
+    "play_count&quot",
+    "total_score&quot",
+    "total_hits&quot",
+    "current&quot",
+]  # из-за конченной системы нахождения отличается по смыслу. лучше бы переписать то как работает "categories"
+
+fort_category = [
+    "Level",
+    "Wins",
+    "Matches",
+    "Kills",
+    "Win rate",
+    "K / D",
+    "Kills / Match",
+]
+
+fort_stats_category = [
+    '"level":"',
+    '"wins":"',
+    '"matches":"',
+    '"kills":"',
+    '"deaths":"',
+]
